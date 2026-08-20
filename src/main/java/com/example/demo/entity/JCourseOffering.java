4a1d9457864d2e4f31501fd73eb89699d252f69e
Mandresy Gaëtan RANDRIANANTOANINA <mandresy2020@gmail.com>
2026-08-20 09:12:22 +0300

    refactor: use UUID generation strategy for entity JCourseOffering IDs

--- src/main/java/com/example/demo/entity/JCourseOffering.java
+++ src/main/java/com/example/demo/entity/JCourseOffering.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 public class JCourseOffering {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
   @ManyToOne(optional = false)
